51db8bda249d0cb4915c0e5067418fabc713d151
maksof-saim <153712387+maksof-saim@users.noreply.github.com>
2025-05-08 18:56:40 +0500

    api-integrate

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,226 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
+"use client"
+import React, { useEffect, useState } from 'react'
+
+
+interface Product {
+    productId: number;
+    productName: string;
+    userEmail: string;
+    userPhone: number;
+}
+
 
 const page = () => {
 
+    const [products, setProducts] = useState<Product[]>([]);
 
-    const phone = [
-        {
-            productName: "",
-            productCompany: "",
-            productColor : "",
-            image : "",
-            price : ""
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('http://localhost:5001/api/products')
+            const data = await response.json()
+            console.log(data, "data")
+            setProducts(data.data)
         }
-    ]
+        fetchProducts()
+    }, [])
 
-    const image = "/images/mobile.jpeg"
 
     return (
-        <div className='main'>
-            <section className="mb-12">
-                <h2 className="text-2xl font-semibold mb-6">Featured Products</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-
-                        <div className="p-4">
-                            <Image
-                                src={image}
-                                alt={"name"}
-                                width={500}
-                                height={200}
-                                className="w-full h-40 object-cover"
-                            />
-                            <h3 className="text-lg font-semibold mb-2 ">first phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
+        <>
+            <div className="main">
+                <section className="mb-12">
+                    <h2 className="text-2xl font-semibold mb-6">Featured Products</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                        {products.map((product) => (
+                            <div
+                                key={product.productId}
+                                className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
+                            >
+                                <h1>{product.productName}</h1>
+                            </div>
+                        ))}
                     </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">second phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">third phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">fourth phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">fifth phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">six phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">seven phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">seven phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">eight phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">nine phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">Ten phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">eleven phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">twelve phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">thirteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">fourteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">fifteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">sixteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">seventeen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">eighteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">nineteen phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                    <div
-                        className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
-                    >
-                        <div className="p-4">
-                            <h3 className="text-lg font-semibold mb-2 ">twenty phone</h3>
-                            <p className="text-gray-600">$phoneprice</p>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        </>
     )
 }
 
